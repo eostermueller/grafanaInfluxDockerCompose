09478cb6cae4da4496056c1345121d7c3bab63d9
Erik Ostermueller <eostermueller@gmail.com>
2017-12-29 14:56:09 -0600

    whoops, missed this.

--- addGrafanaDataSource.sh
+++ addGrafanaDataSource.sh
@@ -1,4 +1,4 @@
-curl -i -u admin:admin -H "Accept:application/json" -H "Content-Type:application/json" -d  @createDataSource.txt http://localhost:3000/api/datasources
+curl -i -u admin:admin -H "Accept:application/json" -H "Content-Type:application/json" -d  @createDataSource.json http://localhost:3000/api/datasources
 
 
 curl -u admin:admin -f http://admin:admin@localhost:3000/api/datasources/name/influxTimeShift
